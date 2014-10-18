bddf323e3d54e0b27db543965045eee77162e5a2
Ben Norris <ben@bsn.io>
2014-10-18 09:12:48 -0600

    New project created

---
+++ NSStringCategory/NSStringCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSStringCategory
+//
+//  Created by Ben Norris on 10/18/14.
+//  Copyright (c) 2014 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
