cf830b8b007fd4b43e8baf1f52af215f4422ec79
Ben Norris <ben@bsn.io>
2014-10-18 09:15:25 -0600

    Added view controller

---
+++ NSStringCategory/NSStringCategory/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NSStringCategory
+//
+//  Created by Ben Norris on 10/18/14.
+//  Copyright (c) 2014 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
